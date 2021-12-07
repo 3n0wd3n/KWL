922efd6c8345449b988209160b17afd0e1d6e33f
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2021-12-07 18:11:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,50 @@
+//select DOM
 
+// --- modals
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+// -- offer
+const academy = document.querySelector('.academy')
+const horse = document.querySelector('.horses')
+const consulting = document.querySelector('.consulting')
+
+//event listeners
+academy.addEventListener('click', academyFunction)
+horse.addEventListener('click', horseFunction)
+consulting.addEventListener('click', consultingFunction)
+
+
+
+function academyFunction(){
+    console.log("academie")
+    var academyDiv = document.querySelector('.content_acad')
+    var horseDiv = document.querySelector('.content_hors')
+    var consultingDiv = document.querySelector('.content_cons')
+    academyDiv.style.display = 'flex';
+    horseDiv.style.display = 'none';
+    consultingDiv.style.display = 'none';
+}
+
+function horseFunction(){
+    console.log("horse")
+    var academyDiv = document.querySelector('.content_acad')
+    var horseDiv = document.querySelector('.content_hors')
+    var consultingDiv = document.querySelector('.content_cons')
+    academyDiv.style.display = 'none';
+    horseDiv.style.display = 'flex';
+    consultingDiv.style.display = 'none';
+}
+
+function consultingFunction(){
+    console.log("consulting")
+    var academyDiv = document.querySelector('.content_acad')
+    var horseDiv = document.querySelector('.content_hors')
+    var consultingDiv = document.querySelector('.content_cons')
+    academyDiv.style.display = 'none';
+    horseDiv.style.display = 'none';
+    consultingDiv.style.display = 'flex';
+}
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
